6e343ece50ad8c78cb76f29a3a122b8024aa0985
Carla Willmes <willmes.carla@gmail.com>
2022-12-19 16:32:50 +0900

    added horizontal line

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,21 @@ import React from 'react'
 
 export default function Card(props) {
   return (
-    <div className='card'>
-      <img src={`/images/${props.item.coverImg}`} alt='' className='card--img' />
-      <div className='card--content'>
-        <div className='card--top'>
-          <img src='/images/location.png' alt='' className='location--img'/>
-          <p className='card--location'>{props.item.location}</p>
-          <p className='card--link'>{props.item.googleMapsUrl}</p>
+    <div>
+      <div className='card'>
+        <img src={`/images/${props.item.coverImg}`} alt='' className='card--img' />
+        <div className='card--content'>
+          <div className='card--top'>
+            <img src='/images/location.png' alt='' className='location--img'/>
+            <p className='card--location'>{props.item.location}</p>
+            <p className='card--link'>{props.item.googleMapsUrl}</p>
+          </div>
+          <h1 className='card--title'>{props.item.title}</h1>
+          <p className='card--date'>{props.item.startDate} - {props.item.endDate}</p>
+          <p className='card--description'>{props.item.description}</p>
         </div>
-        <h1 className='card--title'>{props.item.title}</h1>
-        <p className='card--date'>{props.item.startDate} - {props.item.endDate}</p>
-        <p className='card--description'>{props.item.description}</p>
       </div>
-
+      <hr></hr>
     </div>
   )
 }
