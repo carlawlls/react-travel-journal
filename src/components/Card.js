2e5015c0ec24f1e8f39eaf2985af1003fe140c4c
Carla Willmes <willmes.carla@gmail.com>
2022-12-19 17:03:43 +0900

    brushed up card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ export default function Card(props) {
           <div className='card--top'>
             <img src='/images/location.png' alt='' className='location--img'/>
             <p className='card--location'>{props.item.location}</p>
-            <a href={`${props.item.googleMapsUrl}`} target='_blank' rel='noreferrer' className='card--link'>{props.item.googleMapsUrl}</a>
+            <a href={`${props.item.googleMapsUrl}`} target='_blank' rel='noreferrer' className='card--link'>View On Maps</a>
           </div>
           <h1 className='card--title'>{props.item.title}</h1>
           <p className='card--date'>{props.item.startDate} - {props.item.endDate}</p>
